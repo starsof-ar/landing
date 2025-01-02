76b04443dad1a520c1b9296ac72b07269282a027
rykr <rykr.inquiry@gmail.com>
2025-01-02 09:02:54 -0600

    fixing navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,15 @@
 import Providers from '@components/Providers';
-import Navigation from '@components/Navigation_updated';
-import ModalTrigger from '@components/ModalTrigger';
-import ActionButton from '@components/ActionButton';
-import ModalCreateAccount from '@components/modals/ModalCreateAccount';
+
 import DefaultLayout from '@root/components/page/DefaultLayout';
 import { Suspense } from 'react';
 import DebugGrid from '@components/DebugGrid';
 import DefaultActionBar from '@components/page/DefaultActionBar';
 import Link from 'next/link';
+import Navigation from '@components/Navigation_updated';
+import ModalCreateAccount from '@components/modals/ModalCreateAccount';
+import ModalTrigger from '@components/ModalTrigger';
+import ActionButton from '@components/ActionButton';
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en-us">
@@ -16,7 +18,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           <DefaultLayout previewPixelSRC="https://intdev-global.s3.us-west-2.amazonaws.com/template-app-icon.png">
             <Suspense fallback={<div>Loading...</div>}>
               <br />
-              <Navigation
+              <DebugGrid /> {/* This is the grid that shows the layout of the page */}
+              <DefaultActionBar />
+                      <Navigation
                 logo="✶"
                 logoRightAligned={true}
                 right={
@@ -30,8 +34,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                   </>
                 }
               />
-              <DebugGrid /> {/* This is the grid that shows the layout of the page */}
-              <DefaultActionBar />
+
+
               {children}
             </Suspense>
           </DefaultLayout>
